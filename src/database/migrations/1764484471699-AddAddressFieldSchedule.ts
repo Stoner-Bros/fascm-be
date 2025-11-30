e752f4dcd4e059c2f96986b1551b7cc1775c859d
PhatTV203 <phattv203@gmail.com>
2025-11-30 13:37:05 +0700

    refactor: fix

--- src/database/migrations/1764484471699-AddAddressFieldSchedule.ts
+++ src/database/migrations/1764484471699-AddAddressFieldSchedule.ts
@@ -1,19 +1,13 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddAddressFieldSchedule1764481183918
+export class AddAddressFieldSchedule1764484471699
   implements MigrationInterface
 {
-  name = 'AddAddressFieldSchedule1764481183918';
+  name = 'AddAddressFieldSchedule1764484471699';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "order_schedule" DROP COLUMN "address"`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "order_schedule" ADD "address" character varying`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "harvest_schedule" DROP COLUMN "address"`,
     );
     await queryRunner.query(
       `ALTER TABLE "harvest_schedule" ADD "address" character varying`,
@@ -25,13 +19,7 @@ export class AddAddressFieldSchedule1764481183918
       `ALTER TABLE "harvest_schedule" DROP COLUMN "address"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "harvest_schedule" ADD "address" TIMESTAMP`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "order_schedule" DROP COLUMN "address"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "order_schedule" ADD "address" TIMESTAMP`,
     );
   }
 }
