18684b35e051bbcd6d7125ce737ef2b01f5d1a73
thanhntse <nguyenthaithanh07@gmail.com>
2025-12-21 20:50:41 +0700

    feat: add validation for userId in ApproveRegisterDto

--- src/auth/dto/approve-register.dto.ts
+++ src/auth/dto/approve-register.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
 
 export class ApproveRegisterDto {
   @ApiProperty({ example: 1, type: Number })
+  @IsNumber()
   userId: number;
 }
